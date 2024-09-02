fed69dafefb8fd4c2cebbe410fe33e25bb272b9e
Cyberpradeep <152625492+Cyberpradeep@users.noreply.github.com>
2024-09-02 21:57:36 +0530

    Update encrypt.js

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,1 +1,11 @@
+const crypto = require('crypto');
+
+const encryptData = (data, password) => {
+    const cipher = crypto.createCipher('aes-256-ctr', password);
+    let encrypted = cipher.update(data, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+};
+
+module.exports = { encryptData };
 
