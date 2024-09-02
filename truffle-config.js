e1a91b4bd60a81ef968fc719e30e39958a4f7c76
Cyberpradeep <152625492+Cyberpradeep@users.noreply.github.com>
2024-09-02 21:59:46 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,1 +1,21 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider("YOUR_MNEMONIC", "https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID"),
+      network_id: 4, // Rinkeby's id
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+    },
+  },
+};
 
