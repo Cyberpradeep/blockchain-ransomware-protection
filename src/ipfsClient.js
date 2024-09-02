b1d70b2eb077675c207903992a73fca595d79447
Cyberpradeep <152625492+Cyberpradeep@users.noreply.github.com>
2024-09-02 21:58:22 +0530

    Update ipfsClient.js

--- src/ipfsClient.js
+++ src/ipfsClient.js
@@ -1,1 +1,17 @@
+const ipfsClient = require('ipfs-http-client');
+const fs = require('fs');
+const { encryptData } = require('./encrypt');
+
+const ipfs = ipfsClient.create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' });
+
+const uploadToIPFS = async (filePath, password) => {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const encryptedData = encryptData(data, password);
+    const fileBuffer = Buffer.from(encryptedData, 'utf8');
+
+    const ipfsResult = await ipfs.add(fileBuffer);
+    return ipfsResult.path; // IPFS CID
+};
+
+module.exports = { uploadToIPFS };
 
