20a8dc24fa18e132556e087d893d5636a745f987
Cyberpradeep <152625492+Cyberpradeep@users.noreply.github.com>
2024-09-02 21:57:58 +0530

    Update decrypt.js

--- src/decrypt.js
+++ src/decrypt.js
@@ -1,1 +1,11 @@
+const crypto = require('crypto');
+
+const decryptData = (encryptedData, password) => {
+    const decipher = crypto.createDecipher('aes-256-ctr', password);
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+};
+
+module.exports = { decryptData };
 
