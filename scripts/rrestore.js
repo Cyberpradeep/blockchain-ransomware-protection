2cde9964280a681409b2374a2c99a2f8f8a39e17
Cyberpradeep <152625492+Cyberpradeep@users.noreply.github.com>
2024-09-02 21:57:09 +0530

    Update rrestore.js

--- scripts/rrestore.js
+++ scripts/rrestore.js
@@ -1,1 +1,13 @@
+const fs = require('fs');
+const { restoreFile } = require('../src/blockchainClient');
+
+const main = async () => {
+    const fileHash = 'your-file-hash';
+    const password = 'your-encryption-password';
+
+    await restoreFile(fileHash, password);
+    console.log('File restored successfully.');
+};
+
+main();
 
